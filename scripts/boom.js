07b514010131a559f2c7c34cf550af3e41647199
boomingbrake <brittanywebbe@gmail.com>
2016-04-04 16:03:37 -0400

    Add mobile friendly navigation.

---
+++ scripts/boom.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  // Hover Header
+  $(window).scroll(function() {
+
+    if ($(window).scrollTop() > 100) {
+      $('.header').addClass('header--hover');
+    } else {
+      $('.header').removeClass('header--hover');
+    }
+  });
+
+  // Mobile Navigation
+  $('.mobile-toggle').click(function() {
+    if ($('.header').hasClass('open-nav')) {
+      $('.header').removeClass('open-nav');
+    } else {
+      $('.header').addClass('open-nav');
+    }
+  });
+
+  $('.navigation__link').click(function() {
+    if ($('.header').hasClass('open-nav')) {
+      $('.navigation').removeClass('open-nav');
+      $('.header').removeClass('open-nav');
+    }
+  });
+
+
+  $('.navigation__link').click(function(event) {
+    var id = $(this).attr("href");
+    var offset = 70;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+      scrollTop: target
+    }, 500);
+    event.preventDefault();
+  });
+});

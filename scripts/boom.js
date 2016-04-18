f076c76f915ec7de9467ff48f1bf7d767a20667f
boomingbrake <brittanywebbe@gmail.com>
2016-04-17 21:55:09 -0400

    simple and clean. Spring cleaning

--- scripts/boom.js
+++ scripts/boom.js
@@ -1,39 +1,13 @@
 $(document).ready(function() {
 
-  // Hover Header
+  // Hover Banner
   $(window).scroll(function() {
 
-    if ($(window).scrollTop() > 100) {
-      $('.header').addClass('header--hover');
-    } else {
+    if ($(window).scrollTop() > 10) {
       $('.header').removeClass('header--hover');
-    }
-  });
-
-  // Nave Navigation
-  $('.nav-toggle').click(function() {
-    if ($('.header').hasClass('open-nav')) {
-      $('.header').removeClass('open-nav');
     } else {
-      $('.header').addClass('open-nav');
+      $('.header').addClass('header--hover');
     }
   });
 
-  $('.navigation__link').click(function() {
-    if ($('.header').hasClass('open-nav')) {
-      $('.navigation').removeClass('open-nav');
-      $('.header').removeClass('open-nav');
-    }
-  });
-
-
-  $('.nav--jump').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 70;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-      scrollTop: target
-    }, 500);
-    event.preventDefault();
-  });
 });

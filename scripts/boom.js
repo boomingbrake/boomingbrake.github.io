c6e130102cd2d846f896f8db92b31c3bac9362b7
boomingbrake <brittanywebbe@gmail.com>
2016-04-28 14:07:30 -0400

    Redesign and layout/content modifications

--- scripts/boom.js
+++ scripts/boom.js
@@ -1,12 +1,30 @@
 $(document).ready(function() {
 
-  // Hover Banner
-  $(window).scroll(function() {
-
-    if ($(window).scrollTop() > 10) {
-      $('.header').removeClass('header--hover');
+  $("#extraTab").click(function() {
+    if ($(this).hasClass("pulled")) {
+      $(this).addClass("closed").removeClass("pulled")
+      $("#moreStuff").css({
+        'right': "-250px"
+      })
     } else {
-      $('.header').addClass('header--hover');
+      $(this).removeClass("closed").addClass("pulled")
+      $("#moreStuff").css({
+        'right': "0px"
+      })
+    }
+  });
+
+  $("#headerTab").click(function() {
+    if ($(this).hasClass("header--opened")) {
+      $(this).addClass("header--closed").removeClass("header--opened")
+      $("#headerImg").css({
+        'top': "-250px"
+      })
+    } else {
+      $(this).removeClass("header--closed").addClass("header--opened")
+      $("#headerImg").css({
+        'top': "0px"
+      })
     }
   });
 
